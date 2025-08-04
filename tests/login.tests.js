e0ea5742155b39cc741b2c70f364080bbad6e1a6
Amanda Ruiz <80833331+amandavruiz@users.noreply.github.com>
2025-08-04 14:29:41 -0300

    Adicionando a função de helper para obtenção de token e criando o teste de transferência

--- tests/login.tests.js
+++ tests/login.tests.js
@@ -1,39 +1,27 @@
 import http from 'k6/http'
 import { sleep, check } from 'k6'
+import { obterToken } from '../helpers/autenticacao.js'
 
 export const options = {
     stages: [
         { duration: '10s', target: 10 },
         { duration: '20s', target: 10 },
-        { duration: '10s', target: 30 },        
+        { duration: '10s', target: 30 },
         { duration: '20s', target: 30 },
         { duration: '20s', target: 0 }
     ],
     thresholds: {
-        http_req_duration: ['p(90)<3000','max<5000'],
+        http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
     }
 }
 
 export default function () {
-    const url = 'http://localhost:3000/login'
+    const { status, token } = obterToken()
 
-    const payload = JSON.stringify({
-        username: 'julio.lima',
-        senha: '123456',
-    });
-
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-
-    const res = http.post(url, payload, params)
-
-    check(res, {
-        'Validar que o status é 200': (r) => r.status === 200,
-        'Validar que o token é string': (r) => typeof(r.json().token) === 'string',
+    check({ status, token }, {
+        'Status deve ser 200': (r)=> r.status === 200,
+        'Token deve ser uma string': (r) => typeof r.token === 'string',
     })
     sleep(1)
 }
---
+++ tests/transferencias.tests.js
@@ -0,0 +1,39 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+import { obterToken } from '../helpers/autenticacao.js'
+
+export const options = {
+    stages: [
+        { duration: '10s', target: 10 },
+    ],
+    thresholds: {
+        http_req_duration: ['p(90)<3000','max<5000'],
+        http_req_failed: ['rate<0.01']
+    }
+}
+
+export default function () {
+    const {token} = obterToken()
+
+    const url = 'http://localhost:3000/transferencias'
+
+    const payload = JSON.stringify({
+        contaOrigem: 1,
+        contaDestino: 2,
+        valor: 11,
+    })
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    }
+
+    const res = http.post(url, payload, params)
+
+    check(res, {
+        'Validar que o status é 201': (r) => r.status === 201
+    })
+    sleep(1)
+}
